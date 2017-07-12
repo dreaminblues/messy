1527131475da4f9264aac1f66cbaf383d03d08dc
Arnab <eternalskyblue@gmail.com>
2017-07-12 19:22:56 -0400

    setting up stuff

--- src/main/groovy/org/omf/messy/application/BotApplication.groovy
+++ src/main/groovy/org/omf/messy/application/BotApplication.groovy
@@ -3,14 +3,11 @@ package org.omf.messy.application
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
-/**
- *
- */
 @SpringBootApplication
 class BotApplication {
 
-    static void main(String[] args) {
-        SpringApplication.run(BotApplication.class, args)
+    static void main(String... args) {
+        SpringApplication.run(BotApplication, args)
     }
 
 }
